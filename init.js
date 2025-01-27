c70ddbdc62bdbf338b1788a870e16006179a817f
mirceaculita <culita88@gmail.com>
2025-01-27 10:02:33 +0200

    use dvh for body

--- init.js
+++ init.js
@@ -3,9 +3,6 @@
         element: document.querySelector(".gameWindow")
     });
     gameWindow = document.getElementById('gameWindow'); 
-    setTimeout(() => {
-    document.body.scrollTo(0,1000);
-    }, 200);
     obj.init();
 
 })();
