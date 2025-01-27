7817693db3d604ea0d7d665496eb84954525b28b
mirceaculita <culita88@gmail.com>
2025-01-27 09:50:33 +0200

    add timeout

--- init.js
+++ init.js
@@ -3,7 +3,9 @@
         element: document.querySelector(".gameWindow")
     });
     gameWindow = document.getElementById('gameWindow'); 
+    setTimeout(() => {
     document.body.scrollTo(0,1000);
+    }, 200);
     obj.init();
 
 })();
